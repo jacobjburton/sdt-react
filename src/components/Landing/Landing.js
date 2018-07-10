bbc6ef826dad0ab67d9b7c45e12fb1e973daa21b
Jacob Burton <jacobburton@gmail.com>
2018-07-10 12:18:11 -0600

    navbar setup

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Landing extends Component
+{
+    render()
+    {
+        return (
+            <div id='landing'>
+
+            </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+class NavBar extends Component
+{
+    render()
+    {
+        return (
+            <div id="NavBar">
+                <div id="innerNav">
+                    <span id="titleWrapper">
+                        <h1>
+                            <Link id='title' to='/'>
+                                <img id='logo' src={require('../../images/152px-Nautical_star.svg.png')} alt="logo"/>
+                                Sirius Design & Technology
+                            </Link>
+                        </h1>
+                    </span>
+                    <ul id='navMenu'>
+                        <li><Link className='navLinks' to='/'>Home</Link></li>
+                        <li><Link className='navLinks' to='/about'>About</Link></li>
+                        <li><Link className='navLinks' to='/projects'>Projects</Link></li>
+                        <li><Link className='navLinks' to='/contact'>Contact Info</Link></li>
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
