f28de6322fe439b6637b1e7144233e2cbdefef73
Jacob Burton <jacobburton@gmail.com>
2018-07-12 12:45:18 -0600

    completed setup of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,23 +15,130 @@ class Projects extends Component
                         <div className='projectDetails'>
                             <p>Analog aquisition and multiplexer boards.  These boards provide low noise acquisition of a large number of signals at high speed, format and transmit the data to a digital control board and then to a controlling computer.  The analog signal acquisition board has over 3400 parts and 16 layers with extensive plane and island design to preserve the low noise signals in a noisy digital environment.  Four 676 pin BGA FPGA's provide local control.  Formatted data is transmitted out at 48 Gb/sec.  For more information visit <a href="http://delphinusmt.com" target='_blank' rel='noopener noreferrer'>http://delphinusmt.com</a></p>
                             <div className='projectImages'>
-                                <span>
-                                    <img src={require('../../images/high-end-ultrasound-system/Full Leg First Production shots sm.jpg')} alt=""/>
-                                    <p>Initial Production Shot</p>
-                                </span>
-                                <span>
-                                    <img src={require('../../images/high-end-ultrasound-system/MUX_bd-top-sm2.jpg')} alt=""/>
-                                    <p>Multiplexer Board</p>
-                                </span>
-                                <span>
-                                    <img src={require('../../images/high-end-ultrasound-system/VTB-top-loaded1sm2-rot.jpg')} alt=""/>
-                                    <p>Signal Acquisition Board</p>
-                                </span>
+                                <div className='projectImagesInner'>
+                                    <span>
+                                        <img src={require('../../images/high-end-ultrasound-system/Full Leg First Production shots sm.jpg')} alt=""/>
+                                        <p>Initial Production Shot</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/high-end-ultrasound-system/MUX_bd-top-sm2.jpg')} alt=""/>
+                                        <p>Multiplexer Board</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/high-end-ultrasound-system/VTB-top-loaded1sm2-rot.jpg')} alt=""/>
+                                        <p>Signal Acquisition Board</p>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className='project'>
-                    
+                        <h3>Heartstream XLT Defibrillator</h3>
+                        <div className='projectDetails2'>
+                            <p>System and main controller design.  Overall product validation, test, and regulatory qualification testing.  This was the last product designed by the resuscitation group of Agilent Technologies (previously Hewlett Packard, now Philips) in McMinnville, OR.</p>
+                            <div className='projectImages'>
+                                <div className='projectImagesInner'>
+                                    <span>
+                                        <img src={require('../../images/xltdefibwithcase.jpg')} alt=""/>
+                                        <p>XLT Defibrillator</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='project'>
+                        <h3>Inovise Heart Sound Detection Algorithm</h3>
+                        <div className='projectDetails2'>
+                            <div className='projectImages'>
+                                <div className='projectImagesInner'>
+                                    <span>
+                                        <img src={require('../../images/heartsound-algorithm/HSclipsm.JPG')} alt=""/>
+                                    </span>
+                                </div>
+                            </div>
+                            <div className='projectDetailsInner'>
+                                <p>Developed the heart sound detection algorithm used in the Inovise Audicor product.  This was an extensive DSP algorithm project that involved data collection, biomedical research and sensitivity-specificity evaluation.  You can view the resulting published application at the United States Patent and Trademark Office:  
+                                    <a href="http://appft1.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PG01&s1=20040267148&OS=20040267148&RS=20040267148" target='_blank' rel='noopener noreferrer'> 20040267148 (30Dec04 - Method and System for Detection of Heart Sounds)</a>.  To view Audicor products visit: <a href="https://inovise.com/" target='_blank' rel='noopener noreferrer'>https://inovise.com</a></p>
+                                <p>Received Medical Design Excellence Award in 2004 for development of this algorithm.    
+                                    <img className='excellenceAward' src={require('../../images/heartsound-algorithm/MDEAlogo-noboxsm.JPG')} alt=""/>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='project'>
+                        <h3>Large Industrial Research Test System</h3>
+                        <div className='projectDetails'>
+                            <p>The exact details of this project are proprietary information reserved by the customer.  It has 128 RF channels and puts out about 1.3kW of power.  Responsible for much of the system design and integration as well as design and layout of the multichannel output circuit boards.  This was a project coordinated between 7 local area engineering contractors combining the expertise of Electrical Engineers (analog, digital and RF), Software developers, and Mechanical Engineers.</p>
+                            <p>
+                                <h4>System Features:</h4>
+                                <ul>
+                                    <li>System board size 13" x 14"</li>
+                                    <li>Real time OS with running Windows as an app</li>
+                                    <li>High Performance backplanes</li>
+                                    <li>FPGA interface and control circuits</li>
+                                    <li>128 programmable switching supplies</li>
+                                    <li>128 synchronized DDS</li>
+                                    <li>Rack mounted 2kW power supply</li>
+                                    <li>Matched delay clocks for 16 - 8 channel boards</li>
+                                    <li>Matched delay signal routing for 128 channels</li>
+                                    <li>Power pad through the board thermal management</li>
+                                </ul>
+                            </p>
+                            <div className='projectImages'>
+                                <div className='projectImagesInner'>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/HPAsys.JPG')} alt=""/>
+                                        <p>Full System</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/Rack.JPG')} alt=""/>
+                                        <p>Output Cabling</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/PS-protoBD.JPG')} alt=""/>
+                                        <p>Prototype Circuit</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/8chSigBd.JPG')} alt=""/>
+                                        <p>8-Channel Board</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/8chSigBd-channel.JPG')} alt=""/>
+                                        <p>Close-up of 1-Channel on 8-Channel Board</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/8chSigBd-fpga.JPG')} alt=""/>
+                                        <p>Close-up of FPGA Circuitry on 8-Channel Board</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/2chSigBd-tops.JPG')} alt=""/>
+                                        <p>2 Channel Prototype Board</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/industrial-research-test-system/OutBd.JPG')} alt=""/>
+                                        <p>Output Board</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='project'>
+                        <h3>Small Projects</h3>
+                        <div className='projectDetails3'>
+                            <p>Special Function Compact Flash Interface Card.</p>
+                            <div className='projectImages'>
+                                <div className='projectImagesInner'>
+                                    <span>
+                                        <img src={require('../../images/small-projects/CFcardTop.JPG')} alt=""/>
+                                        <p>Top View of CF Interface Card</p>
+                                    </span>
+                                    <span>
+                                        <img src={require('../../images/small-projects/CFcardBottom.JPG')} alt=""/>
+                                        <p>Bottom View of CF Interface Card</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>                         
                     </div>
                 </div>
             </div>
