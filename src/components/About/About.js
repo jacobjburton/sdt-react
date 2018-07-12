c9c721d20cbade537921ae390afec6e78e9eea08
Jacob Burton <jacobburton@gmail.com>
2018-07-12 11:02:09 -0600

    linked info sheet to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './About.css';
+import pdf from '../../files/SiriusDT-Info.pdf';
 
 class About extends Component
 {
@@ -13,8 +14,19 @@ class About extends Component
                     <br/>
 
                     <div className='collaboration'>
-                        <p>Through close association with firms offering mechanical and software design we are able to offer multidisciplinary services. These services could be small contained portions of a project you need help with, or could range up to full product design and test - from concept to manufacturing. We have combined product design experience of over 80 years, mostly with Hewlett Packard Company designing a variety of medical instruments.</p>
-                        <img className='image' src={require('../../images/mahkeo-222757-unsplash.jpg')} alt=""/>
+                        <p>Through close association with firms offering mechanical and software design we are able to offer multidisciplinary services. These services could be small contained portions of a project you need help with, or could range up to full product design and test - from concept to manufacturing. We have combined product design experience of over 80 years, mostly with Hewlett Packard Company designing a variety of medical instruments. <br/>
+                            <p>View the info sheet: <a className='pdfLink' href={pdf} target="_blank" rel="noopener noreferrer">SiriusDT Info Sheet</a></p>
+                        </p>
+                        <div className='collaborationImage'>
+                            <img className='image' src={require('../../images/MeAtWork.JPG')} alt=""/>
+                            <p>David Burton, President, MSEE</p>
+                        </div>
+                    </div>
+
+                    <br/>
+
+                    <div className='dataSheet'>
+                        
                     </div>
                 </div>
             </div>
